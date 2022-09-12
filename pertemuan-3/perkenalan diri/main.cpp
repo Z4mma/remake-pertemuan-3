f10ec7546f917ea17797414d591f6ccb79822d71
Z4mma <112894676+Z4mma@users.noreply.github.com>
2022-09-12 18:40:56 +0700

    Add files via upload

---
+++ pertemuan-3/perkenalan diri/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 18;
+
+    float t = 169.5;
+
+    char b = 'L';
+    string ub = "XXL";
+    cout << "NAMA : Mizwar Zamma" << endl;
+    cout << "NIM : A11.2022.14137" << endl;
+    cout << "ALAMAT : Jl mayong jepara RT/RW : 06/02" << endl;
+    cout << "ASAL SEKOLAH : MA QUDSIYYAH" << endl;
+    cout << "umur saya : "<< x;
+    cout << endl;
+    cout << "tinggi badan saya : "<< t;
+    cout << endl;
+    cout << "ukuran baju saya : "<< b;
+    cout << endl;
+    cout << "ukuran baju kakak saya : "<< ub;
+    return 0;
+}
